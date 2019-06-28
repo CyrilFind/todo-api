3ee42490a6cbf1b3f7c6ced71fd456c6dc051979
cyrilfind <cyrilfind@gmail.com>
2019-06-28 10:44:40 +0200

    fix: un-mixed up conf of the api-workaroud-activeadmin controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 # Make every controller inherit from this one
-class ApplicationController < ActionController::API
+class ApiController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery unless: -> { request.format.json? }
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 # Use Base ActionController for compatibility with ActiveAdmin
 class ApplicationController < ActionController::Base
-  protect_from_forgery unless: -> { request.format.json? }
-  include DeviseTokenAuth::Concerns::SetUserByToken
 end
