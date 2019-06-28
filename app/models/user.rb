d596e44e1f1bd9d66292a99791fdb99980d54fb8
cyrilfind <cyrilfind@gmail.com>
2019-06-28 10:21:34 +0200

    add recoverable, rememberable and validatable because it was added in the generated migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
