c96d4cbedf3b6b563093b4afc604ea392bbfe0cc
cyrilfind <cyrilfind@gmail.com>
2019-06-29 23:34:21 +0200

    api controller defaults rescues from not_found and invalid with corresponding status

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,4 +3,12 @@ class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::RequestForgeryProtection
   protect_from_forgery unless: -> { request.format.json? }
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { message: e.message }, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { message: e.message }, status: :unprocessable_entity
+  end
 end
