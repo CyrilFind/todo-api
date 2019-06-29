5814987c4f8f06b1798bc873f36ec97f03d90444
cyrilfind <cyrilfind@gmail.com>
2019-06-29 23:33:50 +0200

    fix: include module in api controller to define protect_from_forgery method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 # Make every controller inherit from this one
 class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::RequestForgeryProtection
   protect_from_forgery unless: -> { request.format.json? }
 end
